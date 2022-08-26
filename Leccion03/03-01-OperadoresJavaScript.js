7dd4577f420aa2ae36d100ffefb7aae8267d037f
Adrian <luiscabreraucme@hotmail.com>
2022-08-25 21:51:58 -0500

    Operadores Aritméticos en JavaScript

---
+++ Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la Suma "+z);
+
+z = a - b ;
+console.log("Resultado de la Resta "+ z);
+
+z = a * b;
+console.log("Resultado de la Multiplicación " +z);
+
+z = a / b;
+console.log("Resultado de la División " + z);
+
+z = a % b;
+console.log("Resultado de el residuo de la division " + z);
+
+z = a **b;
+console.log("Resultado del operador exponente " +z)
+
