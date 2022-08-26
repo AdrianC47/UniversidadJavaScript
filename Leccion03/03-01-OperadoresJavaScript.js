57ca93204a93a2df5a51044501d793545f3ab59c
Adrian <luiscabreraucme@hotmail.com>
2022-08-25 22:18:37 -0500

    Precedencia de Operadores en JavaScript

--- Leccion03/03-01-OperadoresJavaScript.js
+++ Leccion03/03-01-OperadoresJavaScript.js
@@ -1,41 +1,53 @@
-let a = 3, b = 2;
-let z = a + b;
-console.log("Resultado de la Suma "+z);
+// let a = 3, b = 2;
+// let z = a + b;
+// console.log("Resultado de la Suma "+z);
 
-z = a - b ;
-console.log("Resultado de la Resta "+ z);
+// z = a - b ;
+// console.log("Resultado de la Resta "+ z);
 
-z = a * b;
-console.log("Resultado de la Multiplicación " +z);
+// z = a * b;
+// console.log("Resultado de la Multiplicación " +z);
 
-z = a / b;
-console.log("Resultado de la División " + z);
+// z = a / b;
+// console.log("Resultado de la División " + z);
 
-z = a % b;
-console.log("Resultado de el residuo de la division " + z);
+// z = a % b;
+// console.log("Resultado de el residuo de la division " + z);
 
-z = a **b;
-console.log("Resultado del operador exponente " +z)
+// z = a **b;
+// console.log("Resultado del operador exponente " +z)
 
-// Incremento
-// Pre-Incremento (el operador ++ antes de la variable)
-z = ++a //primero se incrementa y luego se asigna
+// // Incremento
+// // Pre-Incremento (el operador ++ antes de la variable)
+// z = ++a //primero se incrementa y luego se asigna
+// console.log(z)
+// console.log(a)
+
+// // Post-incremento (el operador ++ despues de la variable)
+// z = b++ //primero se asigna 
+// console.log(z)
+// console.log(b)
+
+// // Decremento
+// // Pre-Decremento 
+// z = --a;
+// console.log(a);
+// console.log(z);
+
+// // Post-Decremento
+// z = b--;
+// console.log(b);
+// console.log(z);
+
+let a = 3, b =2, c =1, d = 4;
+
+let z = a * b + c / d; //las expresiones se evaluan de izquierda a derecha
 console.log(z)
-console.log(a)
 
-// Post-incremento (el operador ++ despues de la variable)
-z = b++ //primero se asigna 
+z = c + a  * b / d; // sin embargo toma prioridad el producto
 console.log(z)
-console.log(b)
 
-// Decremento
-// Pre-Decremento 
-z = --a;
-console.log(a);
-console.log(z);
-
-// Post-Decremento
-z = b--;
-console.log(b);
-console.log(z);
+// se puede modifcar el orden de las operaciones usando parentesis
+ z= (c +a) *b /c;
+ console.log(z)
 
