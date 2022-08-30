d7685b5d845233c43738693472cc155472d4b136
Adrian <luiscabreraucme@hotmail.com>
2022-08-30 09:24:11 -0500

    Ejemplo If/Else en JavaScript

--- Leccion04/04-01-SentenciasControl-UJS.js
+++ Leccion04/04-01-SentenciasControl-UJS.js
@@ -1,8 +1,26 @@
-let condicion = false;
+// let condicion = false;
 
-if (8>3){ 
-    console.log("Condicion verdadera");
-    console.log("Fin del programa");
-} else{
-    console.log("Condición falsa");
+// if (8>3){ 
+//     console.log("Condicion verdadera");
+//     console.log("Fin del programa");
+// } else{
+//     console.log("Condición falsa");
+// }
+
+let numero = 10;
+if( numero ==1){
+    console.log("Numero uno");
+}
+else if (numero ==2){
+    console.log("Numero dos");
+}
+else if (numero == 3){
+    console.log("Numero tres");
+
+} else if(numero ==4){
+    console.log("Numero cuatro");
+
+}
+else {
+    console.log("Número desconocido");
 }
