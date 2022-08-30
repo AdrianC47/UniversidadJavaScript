83fab8e48b5b89cf43dd27978dd9217561809583
Adrian <luiscabreraucme@hotmail.com>
2022-08-30 09:29:46 -0500

    Ejercicio Calculo de la Estacion del Año

--- Leccion04/04-01-SentenciasControl-UJS.js
+++ Leccion04/04-01-SentenciasControl-UJS.js
@@ -7,20 +7,40 @@
 //     console.log("Condición falsa");
 // }
 
-let numero = 10;
-if( numero ==1){
-    console.log("Numero uno");
-}
-else if (numero ==2){
-    console.log("Numero dos");
-}
-else if (numero == 3){
-    console.log("Numero tres");
+// let numero = 10;
+// if( numero ==1){
+//     console.log("Numero uno");
+// }
+// else if (numero ==2){
+//     console.log("Numero dos");
+// }
+// else if (numero == 3){
+//     console.log("Numero tres");
 
-} else if(numero ==4){
-    console.log("Numero cuatro");
+// } else if(numero ==4){
+//     console.log("Numero cuatro");
 
+// }
+// else {
+//     console.log("Número desconocido");
+// }
+
+let mes = 10;
+let estacion;
+if (mes ==1 || mes == 2 || mes ==12){
+    estacion = "Invierno";
 }
-else {
-    console.log("Número desconocido");
+else if (mes ==3 || mes == 4 || mes ==5){
+    estacion = "Primavera";
 }
+else if (mes == 6 || mes == 7 || mes == 8){
+    estacion = "Verano";
+}
+else if (mes == 9 || mes == 10 || mes == 11){
+    estacion = "Otoño";
+}
+else{
+    estacion = "Valor incorrecto";
+}
+
+console.log(estacion);
