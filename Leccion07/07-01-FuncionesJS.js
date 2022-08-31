97c1a7e71f7f9ddd18edcdd6bf9760aa058750cc
Adrian <luiscabreraucme@hotmail.com>
2022-08-31 15:07:28 -0500

    Sumar todos los Argumentos de una funcion JS

--- Leccion07/07-01-FuncionesJS.js
+++ Leccion07/07-01-FuncionesJS.js
@@ -8,11 +8,11 @@
 // miFuncion(8,4);
 
 //Declaracion de la función
-function miFuncion(a,b){
-    return a+b; //si no se pone la palabra return pues JS añade automaticamente la palabra al final de la funcion
-}
-let resultado = miFuncion(4,4);
-console.log(resultado);
+// function miFuncion(a,b){
+//     return a+b; //si no se pone la palabra return pues JS añade automaticamente la palabra al final de la funcion
+// }
+// let resultado = miFuncion(4,4);
+// console.log(resultado);
 
 
 //Funciones Self-Invoking (funciones que se llaman a sí mismas)
@@ -37,14 +37,25 @@ console.log(resultado);
 // console.log(sumar(8,5));  
 
 // Funciones de Tipo Flecha
-const sumarFuncionTipoFlecha = (a,b)=> a+b;
-console.log(sumarFuncionTipoFlecha(8,6));
+// const sumarFuncionTipoFlecha = (a,b)=> a+b;
+// console.log(sumarFuncionTipoFlecha(8,6));
 
-//Parametros es (a,b) en una funcion en tanto que argumentoos es (2,3) en una funcion
-let sumar = function(a=4,b=5){
-    console.log(arguments[0]);
-    console.log(arguments[1]);
-    console.log(arguments[2])
-    return a +b + arguments[2];
-};
-console.log(sumar(3,8,7));  
+// //Parametros es (a,b) en una funcion en tanto que argumentoos es (2,3) en una funcion
+// let sumar = function(a=4,b=5){
+//     console.log(arguments[0]);
+//     console.log(arguments[1]);
+//     console.log(arguments[2])
+//     return a +b + arguments[2];
+// };
+// console.log(sumar(3,8,7));  
+
+let resultado = sumarTodo(5, 4, 13, 10, 25);
+console.log(resultado)
+function sumarTodo(){
+    let suma = 0;
+    for(let i =0; i <arguments.length; i++){
+        suma += arguments[i]; //suma = suma + arguments[i]
+    }
+    return suma;
+}
+console.log(resultado);
