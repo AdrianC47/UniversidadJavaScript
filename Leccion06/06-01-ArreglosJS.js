6f3f041432c63aafe6b20a3f845c73495c6c71fb
Adrian <luiscabreraucme@hotmail.com>
2022-08-31 11:08:00 -0500

    Arreglos en JavaScript

---
+++ Leccion06/06-01-ArreglosJS.js
@@ -0,0 +1,4 @@
+//let autos = new Array('BMW', 'Mercedes Benz', 'Chevrolet');  Forma antigua
+//Se usa const debido a la referencia en memoria para el arreglo  pudiendo modificar el arreglo
+const autos = ['BMW', 'Mercedes Benz', 'Chevrolet'];
+console.log(autos);
