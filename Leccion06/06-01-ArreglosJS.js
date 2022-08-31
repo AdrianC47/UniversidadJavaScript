221c65bd401bdc04d47acd4144846ed327f381c6
Adrian <luiscabreraucme@hotmail.com>
2022-08-31 11:11:44 -0500

    Recorrer los Elementos de un Arreglo en JavaScript

--- Leccion06/06-01-ArreglosJS.js
+++ Leccion06/06-01-ArreglosJS.js
@@ -2,3 +2,10 @@
 //Se usa const debido a la referencia en memoria para el arreglo  pudiendo modificar el arreglo
 const autos = ['BMW', 'Mercedes Benz', 'Chevrolet'];
 console.log(autos);
+
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i++){
+    console.log(i + " "+ autos[i])
+}
