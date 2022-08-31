a8038175230f1682bfeb18405a8a50c7d4f82067
Adrian <luiscabreraucme@hotmail.com>
2022-08-31 11:30:56 -0500

    Palabra Return en funciones de JavaScript

--- Leccion07/07-01-FuncionesJS.js
+++ Leccion07/07-01-FuncionesJS.js
@@ -1,6 +1,15 @@
+// //Hoisting
+// miFuncion(8,52);    
+// //Declaracion de la función
+// function miFuncion(a,b){
+//     console.log("Suma:" + (a+b));
+// }
+// //Llamando a la función
+// miFuncion(8,4);
+
 //Declaracion de la función
 function miFuncion(a,b){
-    console.log("Suma:" + (a+b));
+    return a+b; //si no se pone la palabra return pues JS añade automaticamente la palabra al final de la funcion
 }
-//Llamando a la función
-miFuncion(8,4);
+let resultado = miFuncion(4,4);
+console.log(resultado);
