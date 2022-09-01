0a251612a7a6404b7bd5d4f405536db383d44b7f
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 12:42:13 -0500

    Agregar Metodos a Objetos en JavaScript

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -5,8 +5,12 @@ let persona ={
     nombre:'Adrian',
     apellido: 'Perez',
     email:'aperez@gmail.com',
-    edad:27
+    edad:27,
+    nombreCompleto: function(){
+        return this.nombre + ' ' +this.apellido; //la palabra this es una variable que apunta al objeto
+    }
 }
 
 console.log(persona.email);
 console.log(persona);
+console.log(persona.nombreCompleto());
