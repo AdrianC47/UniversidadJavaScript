38b302c7f000ee02b0bf4e58c5c7334a9670d4bd
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 12:54:36 -0500

    Acceder a las propiedades de los objetos en JavaScript

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -1,3 +1,32 @@
+// let x = 10;
+// console.log(x.lenght)
+
+// let persona ={
+//     nombre:'Adrian',
+//     apellido: 'Perez',
+//     email:'aperez@gmail.com',
+//     edad:27,
+//     nombreCompleto: function(){
+//         return this.nombre + ' ' +this.apellido; //la palabra this es una variable que apunta al objeto
+//     }
+// }
+
+// console.log(persona.email);
+// console.log(persona);
+// console.log(persona.nombreCompleto());
+
+//Segunda Forma de crear un objeto
+
+// let persona2 = new Object();
+// persona2.nombre = 'Carlos';
+// persona2.apellido ='Sanchez';
+// persona2.direccion = 'Calle Vieja';
+// persona2.telf='789';
+
+// console.log(persona2.nombre)
+// console.log(persona2)
+
+// Acceder a las propiedades de los objetos en js
 let x = 10;
 console.log(x.lenght)
 
@@ -12,16 +41,10 @@ let persona ={
 }
 
 console.log(persona.email);
-console.log(persona);
-console.log(persona.nombreCompleto());
+console.log(persona['edad']);
 
-//Segunda Forma de crear un objeto
-
-let persona2 = new Object();
-persona2.nombre = 'Carlos';
-persona2.apellido ='Sanchez';
-persona2.direccion = 'Calle Vieja';
-persona2.telf='789';
-
-console.log(persona2.nombre)
-console.log(persona2)
+// for in recorre cada una de las propiedades del objeto
+for (nombrepropiedad in persona) {
+    console.log(nombrepropiedad);
+    console.log(persona[nombrepropiedad]);
+}
