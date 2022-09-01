80d843959214d0236165e947b83505150767500f
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 17:41:35 -0500

    Métodos Get y Set en JavaScript

--- Leccion09/09-01-Clases.js
+++ Leccion09/09-01-Clases.js
@@ -1,12 +1,28 @@
 class Persona{
     constructor(nombre,apellido){
-        this.nombre=nombre; //en ese momento con el this.nombre se crea el atributo nombre 
-        this.apellido=apellido;
+        this._nombre=nombre; //en ese momento con el this.nombre se crea el atributo nombre 
+        this._apellido=apellido; //se acostumbra a poner el _ debido a que el metodo GET no puede llevar el mismo nombre que el atributo
     }
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
 }
 
 //Si no se define un constructor, JavaScript agrega uno vacío en automático
 let persona1  = new Persona("Bob","Dylan"); 
+
+console.log(persona1.nombre); //GET
+
+persona1._nombre="Jhonny";//SET
+
 console.log(persona1);
+
+
+
 let persona2 = new Persona("John", 'Lennon');
 console.log(persona2);
