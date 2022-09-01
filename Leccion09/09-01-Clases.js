9319e231d3ebabfd80db5c7057f0c69ef381dc51
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 17:47:41 -0500

    Hoisting y Clases en JavaScript

--- Leccion09/09-01-Clases.js
+++ Leccion09/09-01-Clases.js
@@ -1,3 +1,12 @@
+
+/*
+************************************************************************
+En el caso de las clases pues deben estar primero declaradas antes de 
+ser usadas, es decir no usa hoisting que sí se usa en funciones
+************************************************************************
+*/
+// let persona2 = new Persona('Karla', 'Juárez'); No es posible crear objetos antes de declarar la clase
+
 class Persona{
     constructor(nombre,apellido){
         this._nombre=nombre; //en ese momento con el this.nombre se crea el atributo nombre 
@@ -26,3 +35,6 @@ console.log(persona1);
 
 let persona2 = new Persona("John", 'Lennon');
 console.log(persona2);
+
+let persona3 = new Persona('Karla', 'Juárez'); 
+console.log(persona3);
