4dc144c9267b5bd9697eb6b10ab7b1edc4ecc4c0
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 12:36:32 -0500

    Objetos en JavaScript

---
+++ Leccion08/08-01-Objects.js
@@ -0,0 +1,12 @@
+let x = 10;
+console.log(x.lenght)
+
+let persona ={
+    nombre:'Adrian',
+    apellido: 'Perez',
+    email:'aperez@gmail.com',
+    edad:27
+}
+
+console.log(persona.email);
+console.log(persona);
