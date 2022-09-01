8c218419f8ddf9727f4e7d7bfc0b4ebbfc987f34
Adrian <luiscabreraucme@hotmail.com>
2022-09-01 16:39:38 -0500

    Distintas Formas de Crear Objetos en JavaScript

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -127,3 +127,22 @@ console.log(madre.nombreCompleto());
 padre.nombre = 'Luis';
 console.log(padre);
 console.log(padre.nombreCompleto());
+
+//Distintas Formas de Crear Objetos en JavaScript
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String('HOLA');
+let miCadena2 = ('ADIOS');
+
+let miNumero = new Number(1);
+let miNumero2 = 4;
+
+let miBoolean = new Boolean(true);
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+let miArreglo2 =[];
+
+let miFuncion = new Function();
+let miFuncion2 = function(){};
