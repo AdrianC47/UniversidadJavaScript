41be5b13eab2787de3f1e645b6e361a45434f111
Adrian <luiscabreraucme@hotmail.com>
2022-09-02 17:55:09 -0500

    Creacion Clase Persona

---
+++ Leccion09/Ejercicio de Herencia en JS/Persona.js
@@ -0,0 +1,55 @@
+class Persona {
+    static contadorPersonas = 0;
+
+    constructor(idPersona, nombre, apellido, edad){
+        this.nombre= nombre;
+        this.apellido = apellido;
+        this.edad = edad
+        // this.idPersona = ++Persona.contadorPersonas;
+        this.idPersona = idPersona;
+    }
+
+    get getIdPersona(){
+        return this.idPersona;
+    }
+    get getNombre(){
+        return this.nombre;
+    }
+
+    set setNombre(nombre){
+        this.nombre = nombre;
+    }
+
+    get getApellido(){
+        return this.apellido;
+    }
+
+    set setApellido(apellido){
+        this.apellido = apellido;
+    }
+
+    get getEdad(){
+        return this.edad;
+    }
+
+    set setEdad(edad){
+        this.edad = edad;
+    }
+
+    toString(){
+        return `
+                ${this.idPersona}
+                ${this.nombre}
+                ${this.apellido}
+                ${this.edad}` ;
+    }
+}
+
+
+class Empleado extends Persona{
+
+}
+
+class Cliente extends Persona{
+
+}
