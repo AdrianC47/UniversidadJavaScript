b08cede0120690e06411a8644306dc57b5cf3633
Adrian <luiscabreraucme@hotmail.com>
2022-09-06 17:27:03 -0500

    Más de Funciones Flecha en JavaScript

--- Leccion13/13-01-FuncionesFlecha.js
+++ Leccion13/13-01-FuncionesFlecha.js
@@ -2,13 +2,41 @@ let miFuncion = function (){
     console.log('saludos desde mi función');
 }
 
-const miFuncionFlecha = () => {
-    console.log('saludos desde mi función flecha');
+// const miFuncionFlecha = () => {
+//     console.log('saludos desde mi función flecha');
+// }
+
+//const miFuncionFlecha = ()=>console.log('saludos desde mi función flecha');
+
+//miFuncionFlecha();
+//miFuncion();
+// const saludar = () =>{
+//     return 'Saludos desde funcion flecha';
+//}
+
+const saludar = ()=> 'Saludos desde funcion flecha'; 
+
+console.log(saludar());
+
+const regresaObjeto =  () =>({nombre:"Elliot", apellido:"Alderson"}); //si retorno un objeto pues se lo pone entre ({}) debido a que se puede confundir con el cuerpo de una funcion
+console.log(regresaObjeto());
+
+const funcionConParametrosClasico = function(mensaje){
+    console.log(mensaje);
 }
+funcionConParametrosClasico("pepe");
 
-miFuncionFlecha();
-miFuncion();
+// const funcionConParametros = (mensaje) => console.log(mensaje); si es un solo parametro pues se puede omitir los parentesis
+const funcionConParametros = mensaje => console.log(mensaje);
+funcionConParametros("Hello friend");
 
+
+// const funcionConVariosParametros = (op1,op2) => op1+op2;
+const funcionConVariosParametros = (op1,op2) =>{
+    let resultado = op1 + op2;
+    return `Resultado: ${resultado} `;  
+} 
+console.log(funcionConVariosParametros(5,8));
 
 
 
