baf49688e1c1ea4aed5de5652226059f616d7983
Adrian <luiscabreraucme@hotmail.com>
2022-09-06 17:07:58 -0500

    Cláusula Throw en JavaScript

--- Leccion12/12-01-ManejoErrores.js
+++ Leccion12/12-01-ManejoErrores.js
@@ -1,11 +1,29 @@
-"use strict"
+// "use strict"
+// try {
+//     x= 10;    
+//     miFuncion();
+// } catch (error) {
+//     console.log(error);   
+// }
+// finally{ //el bloque finally siempre se va a ejecutar
+//     console.log("termina la revision de errores");
+// }
+// console.log("continuamos");
+
+'use strict'
+let resultado = '4';
+
 try {
-    x= 10;    
-    miFuncion();
+    //x = 10;
+    if (isNaN(resultado)) throw 'No es un número'
+    else if(resultado === '') throw "Es cadena vacia";
+    else if(resultado >= 0) throw "Valor positivo";
+    else if(resultado <0) throw "Valor negativo";
 } catch (error) {
-    console.log(error);   
+    console.log(error);
+    // console.log(error.name);
+    // console.log(error.message);
 }
-finally{ //el bloque finally siempre se va a ejecutar
-    console.log("termina la revision de errores");
+finally{
+    console.log("Termina revision de errores")
 }
-console.log("continuamos");
