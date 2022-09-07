fb4b22ec9a05a33c1c6553f8891cbe8f4959b63e
Adrian <luiscabreraucme@hotmail.com>
2022-09-07 17:37:15 -0500

    Función SetTimeout en JavaScript

--- Leccion14/14-01-FuncionesCallback.js
+++ Leccion14/14-01-FuncionesCallback.js
@@ -1,29 +1,44 @@
-miFuncion2();
-miFuncion1();
+// miFuncion2();
+// miFuncion1();
 
-function miFuncion1(){
-    console.log("funcion 1");
+// function miFuncion1(){
+//     console.log("funcion 1");
+// }
+// function miFuncion2(){
+//     console.log("funcion 2");
+// }
+
+
+// //Funciones de Tipo CallBack, llamar a una funcion dentro de otra funcion como argumento
+
+// //Se define una funcion que posteriormente se utiliza como parametro a otra funcion
+// function imprimir(mensaje){
+//     console.log(mensaje);
+// }
+
+// function division(valor){
+//     console.log(valor/2);
+// }
+
+// function sumar(op1,op2,funcionCallback){
+//     let res = op1 + op2;
+//     funcionCallback(res);
+// }
+
+// sumar(8,5,imprimir);
+// sumar(10,10,division);
+
+// Llamadas asíncronas  con uso setTimeout
+
+function miFuncionCallback (){
+    console.log('saludo asíncrono después de 3 seg');
 }
-function miFuncion2(){
-    console.log("funcion 2");
-}
+
+setTimeout(miFuncionCallback, 3000);//se manda como parametro primero la funcion y luego el tiempo 3000 = 3sg
+
+setTimeout(function()  {console.log("saludo asincrono 2 ")}, 7000);
 
 
-//Funciones de Tipo CallBack, llamar a una funcion dentro de otra funcion como argumento
+setTimeout(function()  {console.log("saludo asincrono 3 ")}, 5000);
 
-//Se define una funcion que posteriormente se utiliza como parametro a otra funcion
-function imprimir(mensaje){
-    console.log(mensaje);
-}
-
-function division(valor){
-    console.log(valor/2);
-}
-
-function sumar(op1,op2,funcionCallback){
-    let res = op1 + op2;
-    funcionCallback(res);
-}
-
-sumar(8,5,imprimir);
-sumar(10,10,division);
+//Aqui se logra evidenciar lo asíncrono pues la ultima funcion se ejecuta antes que la 2da 
