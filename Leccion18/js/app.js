5d84edc7c4c4827e520d5b62e4f1ea9d05748927
Adrian <luiscabreraucme@hotmail.com>
2022-09-14 16:25:48 -0500

    Formulario Agregar nueva Persona con JavaScript

--- Leccion18/js/app.js
+++ Leccion18/js/app.js
@@ -9,6 +9,20 @@ function mostrarPersonas() {
     for (let persona of personas) {
         console.log(persona);
         texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+        console.log( ">O " + texto)
     }
      document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona() {
+    const forma = document.forms['forma'];
+    const nombre = forma.nombre;
+    const apellido = forma.apellido;
+    if (nombre.value != '' && apellido.value != '') {
+        personas.push(new Persona(nombre.value,apellido.value));
+        mostrarPersonas() //vuelvo a recorrer e imprimir mi arreglo de Personas        
+    } else{
+        console.log('No hay información que agregar');
+    }
+
 }
