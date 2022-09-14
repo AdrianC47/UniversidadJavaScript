857d8452a83c1d60744fe72b5cceeca8ab090c38
Adrian <luiscabreraucme@hotmail.com>
2022-09-14 16:05:46 -0500

    Listado de Personas con JavaScript

--- Leccion18/js/app.js
+++ Leccion18/js/app.js
@@ -1,1 +1,14 @@
-console.log("Listado de Personas");
+const personas = [
+    new Persona("Roberto", "García"),
+    new Persona("Luis", "Gonzáles"),
+    new Persona("Gabriel", "León"),
+]
+;
+function mostrarPersonas() {
+    let texto = ' ';
+    for (let persona of personas) {
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+     document.getElementById("personas").innerHTML = texto;
+}
--- Leccion18/js/persona.js
+++ Leccion18/js/persona.js
@@ -1,1 +1,22 @@
-console.log("Clase Persona");
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    get getNombre(){
+        return this.nombre;
+    }
+    
+    set setNombre(nombre){
+        this.nombre=nombre;
+    }
+
+    get getApellido(){ 
+        return this.apellido;
+    }
+
+    set setApellido(apellido){
+        this.apellido = apellido;
+    }
+}
