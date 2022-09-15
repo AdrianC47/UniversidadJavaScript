cb5c1e93acb8f18adb8b3f7346b0bd4b02822d93
Adrian <luiscabreraucme@hotmail.com>
2022-09-15 16:52:34 -0500

    Creación Arreglos  de Ingresos  y Egresos

---
+++ Leccion20/js/app.js
@@ -0,0 +1,18 @@
+const ingresos  =[ 
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta Coche', 1500),
+];
+
+const egresos = [
+    new Egreso('Renta departamento', 900.00),
+    new Egreso('Ropa', 400)
+]; 
+
+let cargarApp = ()=>({
+    cargarCabecero
+});
+
+
+let cargarCabecero = ()=>({
+
+});
